1850a563ee188cb42938f9e9d6acb847f665a48d
Bob2156 <152450330+Bob2156@users.noreply.github.com>
2024-12-24 15:22:47 -0800

    Update interaction.js

--- api/interaction.js
+++ api/interaction.js
@@ -76,3 +76,4 @@ export default async function handler(req, res) {
     console.error('Unsupported interaction type:', interaction.type);
     res.status(400).json({ error: 'Unsupported interaction type' });
 }
+//test
